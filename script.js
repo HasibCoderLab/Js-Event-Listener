e23abea10cdfb157b26815fc81fd191fd72fab2d
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-22 11:01:47 +0600

    write code in the js dfile

---
+++ script.js
@@ -0,0 +1,30 @@
+
+// ============= use EventListener........ ============
+let btns = document.querySelectorAll("button");
+btns.forEach((clickBtn) => {
+    clickBtn.addEventListener("click", () => {
+
+
+        console.log("Hello");
+
+    });
+});
+
+
+// *============== * increment Hello *  + create Hello           ============    
+// **
+// ***
+let allBtns = document.querySelectorAll("button");
+let counter = 1;
+
+btns.forEach((clickBtn) => {
+    clickBtn.addEventListener("click", () => {
+        // clickBtn.innerText = "Amare Click Dise"
+
+        let newH2 = document.createElement("h2");
+        newH2.innerText = `Hello ${counter}`;
+        clickBtn.parentElement.append(newH2);
+        console.log(newH2);
+        counter++;
+    });
+});
